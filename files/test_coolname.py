3fb83ea69730e83a6aed672f11c5ebc2cdff4fd2
ovogelaar <ovogelaar@proxy.nl>
2024-05-31 15:44:09 +0200

    Change to coolname package

---
+++ files/test_coolname.py
@@ -0,0 +1,4 @@
+# test_coolname.py
+import coolname
+
+print(coolname.generate())
--- files/test_requests.py
+++
@@ -1,6 +0,0 @@
-# test_requests.py
-import requests
-
-response = requests.get('https://api.github.com')
-print(response.status_code)
-
